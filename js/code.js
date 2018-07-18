5609906841cdfee143ee3d044d70f2184a74682e
Valentin K <valentin.mail@gmail.com>
2018-07-18 14:30:13 +0300

    add event day and time entry

--- js/code.js
+++ js/code.js
@@ -1,41 +1,44 @@
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-  
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-  
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
+  function countdown(endDate) {
+    let days, hours, minutes, seconds;
+    
+    endDate = new Date(endDate).getTime();
+    
+    if (isNaN(endDate)) {
+    return;
+    }
+    
+    setInterval(calculate, 1000);
+    
+    function calculate() {
+      let startDate = new Date();
+      startDate = startDate.getTime();
+      
+      let timeRemaining = parseInt((endDate - startDate) / 1000);
+      
+      if (timeRemaining >= 0) {
+        days = parseInt(timeRemaining / 86400);
+        timeRemaining = (timeRemaining % 86400);
+        
+        hours = parseInt(timeRemaining / 3600);
+        timeRemaining = (timeRemaining % 3600);
+        
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining = (timeRemaining % 60);
+        
+        seconds = parseInt(timeRemaining);
+        
+        document.querySelector(".days").innerHTML = parseInt(days, 10);
+        document.querySelector(".hours").innerHTML = ("0" + hours).slice(-2);
+        document.querySelector(".minutes").innerHTML = ("0" + minutes).slice(-2);
+        document.querySelector(".seconds").innerHTML = ("0" + seconds).slice(-2);
+      } else {
+        return;
       }
     }
-  
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
   }
   
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('countdown', deadline);
+  (function () { 
+
+    // end date and time of event here
+    countdown('09/09/2018 15:00:00'); 
+  }());
